cde464ed98a860ffabdda009d24646fcbeab625b
Jester <33872552+jiansite@users.noreply.github.com>
2019-06-24 09:38:36 +0800

    修改拦截器

--- src/main/java/com/jester/music/interceptor/LoginInterceptor.java
+++ src/main/java/com/jester/music/interceptor/LoginInterceptor.java
@@ -1,6 +1,5 @@
 package com.jester.music.interceptor;
 
-import com.alibaba.fastjson.JSON;
 import com.jester.music.mode.vo.UserSession;
 import com.jester.music.utils.Result;
 import org.slf4j.Logger;
@@ -12,7 +11,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
+import java.io.IOException;
 
 /**
  * @className: HttpUtil
@@ -59,48 +58,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     public boolean beginControlErupt(String uniqueId, String uri, HttpServletResponse response) {
-        Boolean hasKey = redisTemplate.hasKey(uri+uniqueId);
+        Boolean hasKey = redisTemplate.hasKey(uri + uniqueId);
         if (hasKey) {
             Result result = Result.error(20004, "访问频繁");
-            this.writer(response, result, null);
+            try {
+                response.getWriter().print(result);
+            } catch (IOException e) {
+                logger.info(e.getMessage());
+                return false;
+            }
             return false;
         } else {
             return true;
         }
-    }
-
-    public void writer(HttpServletResponse response, Result result, String sLog) {
-        logger.info(result.toString());
-        String toWrite = JSON.toJSONString(result);
-        this.writer(response, toWrite, sLog);
-    }
-
-    private void writer(HttpServletResponse response, String str, String sLog) {
-        PrintWriter out = null;
-
-        try {
-            logger.info(sLog);
-            response.setCharacterEncoding("UTF-8");
-            response.setContentType("text/plain; charset=utf-8");
-            out = response.getWriter();
-            out.write(str);
-        } catch (Exception e) {
-            logger.error("Exception", e.getMessage());
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (Exception e) {
-                logger.error("Exception", e.getMessage());
-            }
-
-            logger.error(str);
-            if (UserSession.isNull()) {
-                UserSession.clear();
-            }
-
-        }
-
     }
 }
